557cfd516f8330078cfb1558f14b535f673d037e
toekneema <officialtonyma@gmail.com>
2021-05-21 15:36:43 -0400

    added proof of work algo

--- main.go
+++ main.go
@@ -3,19 +3,26 @@ package main
 
 import (
 	"fmt"
-	"/blockchain/block.go"
+	"github.com/toekneema/go_blockchain/blockchain"
+	"strconv"
 )
 
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
+
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}
 }
